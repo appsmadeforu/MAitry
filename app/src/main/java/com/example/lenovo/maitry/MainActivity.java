562db4f8e1f109a5dc6c4f9d32d43656440e2161
maitry chauhan <maitrychauhan1998@gmail.com>
2018-09-11 22:24:36 +0530

    first activity done

--- app/src/main/java/com/example/lenovo/maitry/MainActivity.java
+++ app/src/main/java/com/example/lenovo/maitry/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.lenovo.maitry;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout container;
     AnimationDrawable anim;
+    Button b1;
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
         anim=(AnimationDrawable) container.getBackground();
         anim.setEnterFadeDuration(6000);
         anim.setExitFadeDuration(2000);
+        bindfunction();
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = new Intent(view.getContext(),trans.class);
+                startActivity(i);
+            }
+        });
+    }
+    void bindfunction()
+    {
+        b1=(Button) findViewById(R.id.btn1);
     }
     @Override
     protected void onResume() {
